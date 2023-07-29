2917dec997a0911d45a498f572127a4d92719b4d
daviprios <davipaimrios@gmail.com>
2023-07-29 16:48:00 -0300

    fix: arrow in home not working as intended

--- src/ui/views/Home/index.tsx
+++ src/ui/views/Home/index.tsx
@@ -3,11 +3,8 @@ import { Center, Text,  } from '@chakra-ui/react'
 import Xarrow, { anchorType } from 'react-xarrows'
 
 export default function Home() {
-	const arrowEndAnchorRef = useRef(null)
 	const arrowStartAnchorRef = useRef<HTMLParagraphElement>(null)
 	const [anchorDirection, setAnchorDirection] = useState<anchorType>('auto')
-
-	// const { colorMode } = useColorMode()
 
 	useEffect(() => {
 		setAnchorDirection((window.screen.width - 100 < (arrowStartAnchorRef.current?.getBoundingClientRect().width ?? 0))
@@ -28,15 +25,14 @@ export default function Home() {
 
 	return (
 		<Center minH='full' position={'relative'}>
-			<div ref={arrowEndAnchorRef} style={{
+			<div id={'arrowEndAnchorRef'} style={{
 				position: 'absolute',
 				top: 0,
 				left: '30px'
 			}}></div>
-			<Xarrow start={arrowStartAnchorRef} end={arrowEndAnchorRef} startAnchor={anchorDirection} endAnchor={{ position: 'bottom', offset: { y: 5 }}} dashness={{ strokeLen: 7, nonStrokeLen: 12 }} headSize={5} color='red'/>
-			<Text ref={arrowStartAnchorRef} px='4' textAlign={'center'}>
-				Vamos criar nosso time?
-				Basta ir na seção Pokemon.
+			<Xarrow start={'arrowStartAnchorRef'} end={'arrowEndAnchorRef'} startAnchor={anchorDirection} endAnchor={{ position: 'bottom', offset: { y: 5 }}} dashness={{ strokeLen: 7, nonStrokeLen: 12 }} headSize={5} color='red'/>
+			<Text id={'arrowStartAnchorRef'} px='4' textAlign={'center'} ref={arrowStartAnchorRef}>
+				Vamos criar nosso time? Basta ir na seção Pokemon.
 			</Text>
 		</Center>
 	)
